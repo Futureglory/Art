5eda9827b0f598feafca8f6689d692bf1fbb8681
Futureglory <paulgrace512@gmail.com>
2025-06-16 09:27:44 +0100

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 'use client'
 import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner';
+import Home from './components/Home'
+
 const page = () => {
   return (
     <div className='home-container'>
-      <Navbar/>
-      <Banner/>
+      <Home />
+    
     </div>
   )
 }
